822ae80544b65ce4becb93abe30577a54adc672d
Wojciech Sabat <wojsabat@gmail.com>
2017-04-02 15:58:07 +0200

    implemented point 1, 2 ,3 4 and 5

---
+++ StringCalculator.Tests/StringCalculatorTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        [TestMethod]
+        public void EmptyStringTest()
+        {
+            var numbers = string.Empty;
+            var result = new Calculator().Add(numbers);
+            Assert.AreEqual(0,result);
+        }
+
+        [TestMethod]
+        public void OneNumberTest()
+        {
+            var numbers = "1";
+            var result = new Calculator().Add(numbers);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TwoNumbersTest()
+        {
+            var numbers = "3,4";
+            var result = new Calculator().Add(numbers);
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void MultipleNumbersTest()
+        {
+            var numbers = "3,4,8";
+            var result = new Calculator().Add(numbers);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void NewLineDelimeterTest()
+        {
+            var numbers = "3\n4\n8";
+            var result = new Calculator().Add(numbers);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void DifferentDelimetersTest()
+        {
+            var numbers = "//;\n3;4;8";
+            var result = new Calculator().Add(numbers);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void NegativeNumbersThrowExeptionTest()
+        {
+            var numbers = "//;\n3;-4;8,-12";
+            var exeptionMessage = ""
+
+            try
+            {
+                var result = new Calculator().Add(numbers);
+            }
+            catch (Exception e)
+            {
+                
+            }
+            
+        }
+
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        private string _numbers;
+        private char _delimeter;
+
+        public int Add(string numbers)
+        {
+            if(numbers.Equals(string.Empty))
+                return 0;
+
+            int number;
+            if (int.TryParse(numbers, out number))
+                return number;
+
+            _numbers = numbers;
+
+
+            SetDelimeter();
+
+            var parsedNumbers = ParseMultipleNumbers();
+
+            throw new 
+
+            return parsedNumbers.Sum();
+        }
+
+        private void SetDelimeter()
+        {
+            if (_numbers.StartsWith("//"))
+                HandleCustomDelimeter();
+            else if (_numbers.Contains(","))
+                _delimeter = ',';
+            else
+                _delimeter = '\n';
+        }
+
+        private void HandleCustomDelimeter()
+        {
+            _delimeter = _numbers[2];
+            _numbers = _numbers.Substring(4);
+        }
+
+
+        private IEnumerable<int> ParseMultipleNumbers()
+        {
+            var splittedNumbers = _numbers.Split(_delimeter);
+            var parsedNumbers = splittedNumbers.Select(int.Parse);
+            return parsedNumbers;
+        }
+    }
+}
